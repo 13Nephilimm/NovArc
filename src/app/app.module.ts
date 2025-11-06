1dfad6690fcd51d3aa051417ffdd33ec52c4fe8f
Jeko Kharabadze <jekokharabadze@gmail.com>
2025-11-07 00:36:06 +0400

    footer created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CharacterCardComponent } from './character-card/character-card.componen
 import { CharacterListComponent } from './character-list/character-list.component';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
     CharacterCardComponent,
     CharacterListComponent,
     CharacterDetailsComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
